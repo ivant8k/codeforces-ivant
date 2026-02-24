dd39d212c11c283dcd2bba448f27ad78ba0eee94
ivant8k <13523129@std.stei.itb.ac.id>
2026-02-24 07:22:49 +0700

    Codeforces Roundd 1076 & 1079

---
+++ Contest/Codeforces Round 1079/A.cpp
@@ -0,0 +1,78 @@
+#include <bits/stdc++.h>
+#ifdef LOCAL
+#include <sys/resource.h>
+#endif
+using namespace std;
+
+// --- Type Aliases ---
+using ll = long long;
+using vi = vector<int>;
+using vll = vector<ll>;
+using pii = pair<int, int>;
+using mii = map<int, int>;
+
+// --- Macros ---
+#define pb push_back
+#define mp make_pair
+#define all(x) (x).begin(), (x).end()
+#define sz(x) (int)(x).size()
+#define fi first
+#define se second
+#define endl '\n'
+
+// --- Constants ---
+const ll MOD = 1e9 + 7;
+const ll INF = 1e18;
+
+// --- Solution ---
+int digitSum(ll n) {
+    int sum = 0;
+    while (n > 0){
+        sum += n % 10;
+        n /= 10;
+    }
+    return sum;
+}
+
+void solve() {
+    ll n;
+    cin >> n;
+
+    int count = 0;
+    for (ll y = n + 1; y <= n + 90; y++){
+        if (y - digitSum(y) == n){
+            count++;
+        }
+    }
+    cout << count << endl;
+}
+
+// --- Debug ---
+void print_debug() {
+#ifdef LOCAL
+    struct rusage r;
+    getrusage(RUSAGE_SELF, &r);
+    cerr << "\n-----------------------------------" << endl;
+    cerr << "Time   : " << (double)clock() / CLOCKS_PER_SEC << " s" << endl;
+    cerr << "Memory : " << r.ru_maxrss / 1024.0 << " MB" << endl;
+    cerr << "-----------------------------------" << endl;
+#endif
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+#ifdef LOCAL
+    if (fopen("input.txt", "r")) freopen("input.txt", "r", stdin);
+#endif
+
+    int t = 1;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+
+    print_debug();
+    return 0;
+}
